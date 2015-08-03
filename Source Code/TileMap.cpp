e042d7ac0c0bcf45c975408b7a6f493ab66cc4df
NexusValentyne <Nexus-0@outlook.fr>
2015-08-03 12:51:34 -0400

    optimized  compatibility with MinGW + added relative paths for ressources

--- Source Code/TileMap.cpp
+++ Source Code/TileMap.cpp
@@ -13,11 +13,11 @@
 using namespace sf;
 TileMap::TileMap()
 {
-    
+
 }
 bool TileMap::loadFromFile(std::string filePath)
 {
-    std::ifstream flux(filePath,std::ios::in);
+    std::ifstream flux(filePath.c_str());
     if (flux)//If file was loaded correctly
     {
         std::vector<int> temp;
@@ -36,7 +36,7 @@ bool TileMap::loadFromFile(std::string filePath)
                 else//If it detect a 0 in the file it push the temp and start a new line
                 {
                     atemp=false;
-                    
+
                 }
                 if(flux.eof())//Stop the loop if we are at the end of the file
                 {
@@ -46,7 +46,7 @@ bool TileMap::loadFromFile(std::string filePath)
             }
             vmap.push_back(temp);//Push_back the temp vector into the vmap vector
             temp.clear();//Delete every value in temp so we can fill it again with next row of value
-            
+
         }
         return true;//Every thing was ok !
     }
@@ -60,7 +60,7 @@ bool TileMap::loadFromFile(std::string filePath)
 
 bool TileMap::load(std::string textureFile,Vector2u tileSize)
 {
-    
+
     if(!m_texture.loadFromFile(textureFile)) //Load the texture
     {
         return false;//Return false if the texture was not loaded correctly
@@ -81,7 +81,7 @@ bool TileMap::load(std::string textureFile,Vector2u tileSize)
 
             //The number/letter of the actual tile
             int tileNumber = vmap[j][i];
-            
+
             //Set the position of each 4 corner of every tile
             currentVertex[0].position = Vector2f(i*tileSize.x,j*tileSize.y);
             currentVertex[1].position = Vector2f((i+1)*tileSize.x,j*tileSize.y);
@@ -90,13 +90,13 @@ bool TileMap::load(std::string textureFile,Vector2u tileSize)
             // find its position in the tileset texture
             int tu = tileNumber % (m_texture.getSize().x / tileSize.x);
             int tv = tileNumber / (m_texture.getSize().x / tileSize.x);
-            
+
             // define its 4 texture coordinates
             currentVertex[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
             currentVertex[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
             currentVertex[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
             currentVertex[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
-            
+
         }//for end
     }//for end
     }//if end
@@ -109,20 +109,20 @@ bool TileMap::load(std::string textureFile,Vector2u tileSize)
             {
                 //Pointer to the currentVertex used in m_VertexArray
                 Vertex* currentVertex = &m_VertexArray[(j+i*width)*4];
-                
+
                 //The number of the actual tile
                 int tileNumber = vmap[i][j];
-                
+
                 //Set the position of each 4 corner of every tile
                 currentVertex[0].position = Vector2f(j*tileSize.x,i*tileSize.y);
                 currentVertex[1].position = Vector2f((j+1)*tileSize.x,i*tileSize.y);
                 currentVertex[2].position = Vector2f((j+1)*tileSize.x,(i+1)*tileSize.y);
                 currentVertex[3].position = Vector2f(j*tileSize.x,(i+1)*tileSize.y);
-                
+
                 // find its position in the tileset texture
                 int tu = tileNumber % (m_texture.getSize().x / tileSize.x);
                 int tv = tileNumber / (m_texture.getSize().x / tileSize.x);
-                
+
                 // define its 4 texture coordinates
                 currentVertex[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
                 currentVertex[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
@@ -137,7 +137,7 @@ void TileMap::clear()//Usefull to use with a real time map changing
 {
     m_VertexArray.clear();
     vmap.clear();
-    
+
 }
 std::vector<std::vector<int> > TileMap::getVmap()
 {
